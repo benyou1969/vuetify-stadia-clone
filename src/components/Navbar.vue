7282321a3a02cdf3e4ab5665df072d9cf3221466
benyou1969 <benyou.11@hotmail.com>
2020-06-13 14:51:37 +0100

    Finish Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,10 @@
 <template>
-  <v-app-bar app color="primary" dark>
-    <div class="d-flex align-center">
+  <v-app-bar app color="white" flat>
+    <div class="d-flex align-center ml-5">
       <v-img
         alt="stadia Logo"
-        class="shrink mr-2"
         contain
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        :src="smallLogo"
         transition="scale-transition"
         width="40"
       />
@@ -17,29 +16,51 @@
         min-width="100"
         :src="logo"
         width="100"
+        style="margin-left: -32px;"
       />
     </div>
 
     <v-spacer></v-spacer>
 
-    <v-btn
-      href="https://github.com/vuetifyjs/vuetify/releases/latest"
-      target="_blank"
-      text
-    >
-      <span class="mr-2">Latest Release</span>
-      <v-icon>mdi-open-in-new</v-icon>
+    <v-btn elevation text color="#de3334">
+      <span class="mr-2 text-none">About Stadia</span>
+    </v-btn>
+    <v-btn elevation text>
+      <span class="mr-2 text-none">Games</span>
+    </v-btn>
+
+    <v-spacer></v-spacer>
+
+    <v-btn elevation text color="#de3334">
+      <span class="mr-2 text-none">Sign in</span>
+    </v-btn>
+    <v-btn class="btn-gradient" depressed>
+      <span class="mr-2 text-none white--text">Try now</span>
     </v-btn>
   </v-app-bar>
 </template>
 
 <script>
-import Logo from '../assets/logo.svg'
+import Logo from "../assets/text-logo.svg";
+import SmallLogo from "../assets/logo.svg";
 export default {
   name: "Navbar",
 
   data: () => ({
-    logo: Logo
+    logo: Logo,
+    smallLogo: SmallLogo
   })
 };
 </script>
+
+<style>
+.btn-gradient {
+  background: rgb(248, 72, 29);
+  background: linear-gradient(
+    90deg,
+    rgba(248, 72, 29, 1) 0%,
+    rgba(220, 25, 95, 1) 100%,
+    rgba(0, 212, 255, 1) 100%
+  );
+}
+</style>
